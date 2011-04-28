3a7ae70c97dca97c5f74d5d38415eec19bfdbf35
Dilip Gudlur <gdilip@gmail.com>
2011-04-28 00:44:04 -0400

    updated gitignore and aud

--- gen/ds/android/tasknet/R.java
+++ gen/ds/android/tasknet/R.java
@@ -14,19 +14,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int AbsoluteLayout01=0x7f060003;
-        public static final int LinearLayout01=0x7f060009;
-        public static final int btnConfigure=0x7f060002;
-        public static final int btnExit=0x7f060000;
-        public static final int btnExitTaskNet=0x7f06000a;
-        public static final int btnTaskAdv=0x7f06000b;
-        public static final int distributeGlobalButton=0x7f060006;
-        public static final int distributeLocalButton=0x7f060007;
-        public static final int exitButton=0x7f060008;
-        public static final int methodName=0x7f060004;
-        public static final int taMsgs=0x7f06000c;
-        public static final int taskLoad=0x7f060005;
-        public static final int txtNodeName=0x7f060001;
+        public static final int AbsoluteLayout01=0x7f050003;
+        public static final int LinearLayout01=0x7f050009;
+        public static final int btnConfigure=0x7f050002;
+        public static final int btnExit=0x7f050000;
+        public static final int btnExitTaskNet=0x7f05000a;
+        public static final int btnTaskAdv=0x7f05000b;
+        public static final int distributeGlobalButton=0x7f050006;
+        public static final int distributeLocalButton=0x7f050007;
+        public static final int exitButton=0x7f050008;
+        public static final int methodName=0x7f050004;
+        public static final int taMsgs=0x7f05000c;
+        public static final int taskLoad=0x7f050005;
+        public static final int txtNodeName=0x7f050001;
     }
     public static final class layout {
         public static final int configure_node=0x7f030000;
@@ -34,11 +34,8 @@ public final class R {
         public static final int main=0x7f030002;
         public static final int tasknet=0x7f030003;
     }
-    public static final class raw {
-        public static final int aud=0x7f040000;
-    }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
     }
 }
